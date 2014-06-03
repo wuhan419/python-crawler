42c0d0fc07cb5f347508dc023e0efc706ca9187e
wuhan419 <wuhan419@gmail.com>
2014-06-03 14:59:36 +0800

    Update DAO.py
    read config.ini setting database connect

--- DAO.py
+++ DAO.py
@@ -1,23 +1,24 @@
 __author__ = 'wuhan'
 import mysql.connector
 import copy
+from configparser import ConfigParser
 
-#FIXME 文件名统一使用小写和下划线命名 类名查看下编码规范 一般应该也是小写 class关键字上边应该空两行
-class DAO(object):
-    #TODO 写入配置文件 基类应该传入db的配置信息 不要写死了
+
+class Dao(object):
     def __init__(self):
-        self.__user = 'avoper'
-        self.__pwd = '******'
-        self.__db_host = '127.0.0.1'
-        self.__db = 'avdb'
+        config = ConfigParser()
+        config.read("config.ini")
+        self.__user = config.get("dbconfig", "user")
+        self.__pwd = config.get("dbconfig", "pwd")
+        self.__db_host = config.get("dbconfig", "db_host")
+        self.__db = config.get("dbconfig", "db")
 
     def execute_dml(self, sql):
-
         """
-    execute sql update and insert
-    :param sql:
-    :return:
-    """
+        execute sql update and insert
+        :param sql:
+        :return:
+        """
 
         cnx = mysql.connector.connect(user=self.__user, password=self.__pwd, host=self.__db_host, database=self.__db)
         print(sql)
@@ -32,7 +33,6 @@ class DAO(object):
         cnx.commit()
         cursor.close()
         cnx.close
-
 
     def execute_query(self, sql):
         """
@@ -62,10 +62,7 @@ class DAO(object):
             log_sql = open('test.log', 'a')
             log_sql.write("Error: {} \n in the insert/update sql :{}".format(sql_err.msg, sql))
             log_sql.close()
-
             #cursor.close()
 
-
 if __name__ == '__main__':
-    Dao = DAO()
-    Dao.execute_query("select * from av_tag")
+    Dao = Dao()
